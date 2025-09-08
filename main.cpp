d0e18ac15c9920ad9678e8cc233e42827343d7c1
dpantaleoni <dominikpantaleoni@gmail.com>
2025-09-08 12:32:17 -0700

    resolved Handle NaN errors, closes#62

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using std::cin;
 using std::cout;
@@ -8,14 +9,16 @@ using std::endl;
 int main()
 {
     cout << "Hi, please enter two whole numbers: ";
-
+	
     int x,y;
 
     cin >> x >> y;
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
+    cout << "Division: " <<
+    (y == 0 ? "Dividing by zero is not a number." : std::to_string(x / y)) 
+    << endl;
     cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
